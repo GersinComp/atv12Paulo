07b3efc19ef260ff9952d520eb5594e904b9a696
Gerson f. Ribeiro <128738684+GersinComp@users.noreply.github.com>
2024-06-10 20:55:47 -0300

    Esboço
    Primeiros passos na resolução da atividade

---
+++ atv12Paulo.hs
@@ -0,0 +1,36 @@
+olaMundo :: String
+olaMundo = "Alo mundo!"
+
+calcuCirc :: Float->Float
+calcuCirc x = x*x * 3.14
+
+calcularJuros :: Float->Float->Float->Float
+calcularJuros c i t = c*i*t
+
+maxNum :: Float->Float->Float
+maxNum x y
+	| x>y = x
+	| otherwise = y
+	
+verificarNum :: Float->Float
+verificarNum x
+	| x>0 = 1
+	| x<0 = -1      
+	| otherwise = 0
+	
+-- delta :: Float -> Float -> Float -> [Float]
+-- delta a b c = [(-b + sqrt ((b * b) - (4 * a * c))) / (2 * a), (-b - sqrt sqrt ((b * b) - (4 * a * c))) / (2 * a)]
+
+fatorial :: Float->Float
+fatorial 1 = 1
+fatorial x = x * fatorial (x-1)
+
+fibonnacci :: Int -> Int
+fibonnacci 0 = 0
+fibonnacci 1 = 1
+fibonnacci n = fibonnacci(n - 1) + fibonnacci (n - 2)
+
+fibonnacciLista :: Int -> [Int]
+fibonnacciLista 0 = 0:[]
+fibonnacciLista 1 = 1:[]
+fibonnacciLista n = (fibonnacci(n - 1) + fibonnacci (n - 2)):[]
