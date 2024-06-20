c70a5d705252aff1dd70d59608f6951ff6490c59
Gerson f. Ribeiro <128738684+GersinComp@users.noreply.github.com>
2024-06-20 19:04:18 -0300

    Update atv12Paulo.hs
    commit Algoritmo de Euclides

--- atv12Paulo.hs
+++ atv12Paulo.hs
@@ -17,27 +17,33 @@ maxNum :: Float->Float->Float
 maxNum x y
 	| x>y = x
 	| otherwise = y
-
- 
+	
+	
 	
 verificarNum :: Float->Float
 verificarNum x
 	| x>0 = 1
 	| x<0 = -1      
 	| otherwise = 0
-
- 
+	
+	
 	
 delta :: Float -> Float -> Float -> (Float, Float)
 delta a b c
     | ((b^2)- (4 * a * c)) < 0 = error "Nao existem raizes reais"
     | otherwise = ((-b + sqrt((b^2)- (4 * a * c))) / (2 * a), (-b - sqrt((b^2)- (4 * a * c))) / (2 * a))
+	
 
-    
-    
-fatorial :: Float->Float
+
+fatorial :: Float -> Float
 fatorial 1 = 1
 fatorial x = x * fatorial (x-1)
+
+
+calcMDC :: Int -> Int -> Int
+calcMDC a 0 = a                
+calcMDC a b = calcMDC b (a `mod` b)
+
 
 
 
@@ -52,7 +58,6 @@ fibonnacciLista :: Int -> [Int]
 fibonnacciLista 0 = 0:[]
 fibonnacciLista 1 = 1:[]
 fibonnacciLista n = (fibonnacci(n - 1) + fibonnacci (n - 2)):[]
-
 
 
 
